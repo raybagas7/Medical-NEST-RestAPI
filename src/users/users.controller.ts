b3e2e473893ec44c33fa403067f1fb81a20ac471
raybagas7 <raysam.gas@gmail.com>
2024-05-06 17:18:54 +0700

    feat(user controller):
    - add snippet/getting ready for user route and all method
    - create get all user by role, get user by id, post, patch & delete
    
    feat(user service):
    
    - create logic for how user service works to return user data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() // Get .users or /users?role=value
+  findAll(@Query('role') role?: 'PATIENTS' | 'DOCTOR') {
+    return [];
+  }
+
+  @Get('patients')
+  findAllPatients() {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,87 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Ray Samudra',
+      email: 'ray@aug.biz',
+      role: 'PATIENT',
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      email: 'Shanna@doct.tv',
+      role: 'DOCTOR',
+    },
+    {
+      id: 3,
+      name: 'Galih Ramadhan',
+      email: 'galih@yesenia.net',
+      role: 'PATIENT',
+    },
+    {
+      id: 4,
+      name: 'Ryan Permana',
+      email: 'ryandita@kory.org',
+      role: 'DOCTOR',
+    },
+    {
+      id: 5,
+      name: 'Annie Arina',
+      email: 'arina@annie.ca',
+      role: 'PATIENT',
+    },
+  ];
+
+  findAll(role: 'PATIENT' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+
+    return this.users;
+  }
+
+  findOned(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+
+  create(user: { name: string; email: string; role: 'PATIENT' | 'ADMIN' }) {
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role: 'PATIENT' | 'ADMIN';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updatedUser };
+      }
+      return user;
+    });
+
+    return this.findOned(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOned(id);
+
+    this.users = this.users.filter((user) => user.id !== id);
+
+    return removedUser;
+  }
+}
