45f1119cb720e21f5f221c950bb1647e27fdb957
raybagas7 <raysam.gas@gmail.com>
2024-05-06 19:05:42 +0700

    feat(user controller & services):
    - update types user and update user with dto class

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,9 @@ import {
   ParseIntPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -25,23 +28,17 @@ export class UsersController {
   }
 
   @Post() // POST /users
-  create(
-    @Body() user: { name: string; email: string; role: 'PATIENT' | 'DOCTOR' },
-  ) {
-    return this.usersService.create(user);
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 
   @Patch(':id') // PATCH /users/:id
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body()
-    userUpdate: {
-      name?: string;
-      email?: string;
-      role: 'PATIENT' | 'DOCTOR';
-    },
+    updateUserDto: UpdateUserDto,
   ) {
-    return this.usersService.update(id, userUpdate);
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id') // DELETE /users/:id
